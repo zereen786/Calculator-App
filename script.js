c955d1b699a5500cb950c37e6b8cb9375eb13596
Ayshu <ayshuzereen@gmail.com>
2025-03-28 16:39:13 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let display = document.getElementById("display");
+function appendValue(value) {
+    display.value += value;
+}
+function clearDisplay() {
+    display.value = "";
+}
+function backspace() {
+    display.value = display.value.slice(0, -1);
+}
+function calculateResult() {
+    try {
+        let result = new Function("return " + display.value)();
+    } catch {
+        display.value = "Error";
+    }
+}
+document.addEventListener("keydown", (event) => {
+    let key = event.key;
+
+    if ("0123456789+-*/.%".includes(key)) {
+        appendValue(key);
+    } else if (key === "Enter") {
+        calculateResult();
+    } else if (key === "Backspace") {
+        backspace();
+    } else if (key === "Escape") {
+        clearDisplay();
+    }
+});
+
+
